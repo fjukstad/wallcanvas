c98af4a8ed666094d2e0fe229cad061b91ff2788
Bjørn Fjukstad <bfj001@post.uit.no>
2014-03-08 11:36:28 +0100

    Updated pivis.
    Connecting circles, can now connect the d clostest circles. Still using d=1 in
    the visualization, but it is possible to set d to whatever you want.

--- vis.py
+++ vis.py
@@ -1,14 +1,16 @@
 import pygame
 import random
 import threading
+import math
 from wallcanvas import Wallcanvas
+
 
 boxw = 5
 boxh = 5
 
-spacex = 15
-spacey = 15
-radius = 2
+spacex = 17
+spacey = 17
+radius = 3
 
 
 class Vis(Wallcanvas):
@@ -36,8 +38,7 @@ class Vis(Wallcanvas):
 
         self.screen.fill((r,g,b))
         self.draw_pi()
-        #self.draw_box()
-        #self.draw_mazda()
+
     def draw_pi(self):
         x = spacex
         y = spacey
@@ -45,6 +46,8 @@ class Vis(Wallcanvas):
         for l in range(len(self.pi)):
 
             if y > self.height - spacey:
+                #self.write_result(l)
+                print "Number of decimals "+ str(l)
                 break
 
 
@@ -52,18 +55,9 @@ class Vis(Wallcanvas):
 
             square = pygame.Rect(x,y,boxw,boxh)
             color = get_color(letter)
-            #pygame.draw.rect(self.screen, color, square, 0)
 
             pygame.draw.circle(self.screen, color, (x,y), radius, 0)
-            #self.screen.set_at((x,y),color)
-
-
             self.circle_coordinates[(x,y)] = letter
-
-            #x += boxw + spacex
-            #if x > self.width:
-            #    y += boxh + spacey
-            #    x = 0
 
             x += spacex
             if x > self.width - spacex:
@@ -73,11 +67,21 @@ class Vis(Wallcanvas):
         for x, y in self.circle_coordinates:
             self.connect_circles(x,y)
             #pass
+    def write_result(self, digits):
+        color = (242,169,0,0)
+	font = pygame.font.SysFont("Helvetica Light", 30 )
+        text = font.render("The first "+str(digits)+" decimals of pi", 1,color)
+        textpos = text.get_rect()
+        textpos.centerx = self.screen_rect.centerx
+        textpos.centery = self.height - 30
+        self.screen.blit(text, textpos)
 
     def connect_circles(self,x,y):
 
         circle_color = self.circle_coordinates[(x,y)]
-        neighbors = self.get_neighbors(x,y)
+        d = 1
+        neighbors = self.get_neighbors(x,y,d)
+
         n_color = 0
         for n in neighbors:
             try:
@@ -85,18 +89,33 @@ class Vis(Wallcanvas):
             except KeyError:
                 continue
             if n_color == circle_color:
-                pygame.draw.line(self.screen, get_color(n_color), n,(x,y),1)
+                color = get_color(n_color)
 
-    def get_neighbors(self, x,y):
-        neighbors = [
-                (x-spacex, y-spacey),
-                (x, y-spacey),
-                (x+spacex, y-spacey),
-                (x-spacex, y),
-                (x+spacex, y),
-                (x-spacex, y+spacey),
-                (x, y+spacey),
-                (x+spacex, y+spacey)]
+                # get distance and set thickness accordingly
+                d = int(neighbors[n])
+
+                if d <= spacex * 1.5:
+                    t = 2
+                else:
+                    t = 1
+
+                pygame.draw.line(self.screen, color, n,(x,y),t)
+
+
+    # d = distance from circle. 1 = closest neighbors, 2 = one hop out etc.
+    def get_neighbors(self, x,y, d):
+        startx = x - (spacex*d)
+        starty = y - (spacex*d)
+        endx = x + (spacex*d)
+        endy = y + (spacey*d)
+
+        neighbors = {}
+
+        for i in range(startx,endx+spacex,spacex):
+            for j in range(starty,endy+spacey,spacey):
+                d = math.floor(math.sqrt((x - i)**2 + (y-j)**2))
+                neighbors[(i,j)] = d
+
         return neighbors
 
     def draw_box(self):
@@ -134,29 +153,31 @@ class Vis(Wallcanvas):
 
 
 
-def get_color(letter):
+def get_color(letter,a=0):
     if letter == "0":
-        return pygame.Color(239,0,108,0)
+        return pygame.Color(239,0,108,a)
     elif letter == "1":
-        return pygame.Color(238,40,33,0)
+        return pygame.Color(238,40,33,a)
     elif letter == "2":
-        return pygame.Color(252,85,0,0)
+        return pygame.Color(252,85,0,a)
     elif letter == "3":
-        return pygame.Color(254,184,0,0)
+        return pygame.Color(254,184,0,a)
     elif letter == "4":
-        return pygame.Color(255,250,0,0)
+        return pygame.Color(255,250,0,a)
     elif letter == "5":
-        return pygame.Color(137,208,0,0)
+        return pygame.Color(137,208,0,a)
     elif letter == "6":
-        return pygame.Color(54,185,35,0)
+        return pygame.Color(54,185,35,a)
     elif letter == "7":
-        return pygame.Color(18,166,255,0)
+        return pygame.Color(18,166,255,a)
     elif letter == "8":
-        return pygame.Color(28,35,140,0)
+        return pygame.Color(28,35,140,a)
     else:
-        return pygame.Color(136,12,136,0)
+        return pygame.Color(136,12,136,a)
 
 if __name__ == "__main__":
+    #h = 314
+    #w = 716
     h = 3144
     w = 7168
     vis = Vis(w,h)
